607341c389afebfb8cc76a76906bef5a0e1b01fe
Landwixxx <landwixxx@gmail.com>
2022-11-14 00:02:00 -0300

    Teste finalizado

--- server/mysql.js
+++ server/mysql.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql');
 
 var pool = mysql.createPool({    
-    "host": process.env.HOST,
-    "port": process.env.PORT,
-    "driver": process.env.DRIVER,
-    "database": process.env.DATABASE,
-    "user": process.env.USER,
-    "password": process.env.PASSWORD,
-    "name": process.env.NAME    
+    "host": "localhost",
+    "port": 3306,
+    "driver": "MySQL",
+    "database": "patients-db",
+    "user": "root",
+    "password": "controle*302109",
+    "name": "db"    
 });
 
 
